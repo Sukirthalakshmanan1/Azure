c45cb7781c8329e26439d8c9ae526522dd362bbf
Sukirthalakshmanan <sukirtha3700@gmail.com>
2022-11-01 19:59:51 +0530

    Add project files.

---
+++ Practice_sql.sql
@@ -0,0 +1,6 @@
+use School
+
+create table student(student_id int,student_name varchar(20),student_class int)
+insert into student values(1,'suki',1)
+create table Class1(Room_no int,class_strength int)
+create table subjects(subjects_id int,subjects_name varchar(20))
---
+++ query.sql
@@ -0,0 +1,9 @@
+use Meesho
+create table customer(custid int primary key,custname varchar(20))
+insert into customer values(1,'suki')
+insert into customer values(2,'sam')
+select * from customer
+
+SELECT CONNECTIONPROPERTY('local_net_address')
+select * from sys.dm_exec_connections
+
